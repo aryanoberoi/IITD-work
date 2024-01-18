955baae8a63325c8e4683cd29ac0564a4b8ce458
Aryan Oberoi <1MS20IS025@msrit.edu>
2024-01-18 20:30:23 +0530

    LIBRARY

---
+++ library/main.py
@@ -0,0 +1,8 @@
+
+from test_library import Multiplication
+
+# Instantiate a Multiplication object
+multiplication = Multiplication(2)
+
+# Call the multiply method
+print(multiplication.multiply(5))
