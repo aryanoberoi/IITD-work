5cc484c8ec4a52b04c295aad964f93c9987369a4
Aryan Oberoi <1MS20IS025@msrit.edu>
2024-02-09 06:34:17 +0530

    Final library deployed

--- library/main.py
+++ library/main.py
@@ -1,4 +1,59 @@
-from test_library.myfunctions import analyze_sentiment
-input_text = "I love using Hugging Face Transformers library!"
-predicted_sentiment, confidence = analyze_sentiment(input_text)
-print(f"Sentiment: {predicted_sentiment}, Confidence: {confidence}")
+import re
+
+def decode(message_file):
+   
+    data_dict = {}
+
+   
+    with open(message_file, 'r') as file:
+       
+        for line in file:
+            
+            matches = re.findall(r'(\d+\.?\d*)\s*([a-zA-Z]+)', line)
+
+           
+            for num_str, string in matches:
+                num = int(num_str)
+                data_dict[num] = string
+
+    
+
+
+    line_count = 0
+
+    with open(message_file, 'r') as file:
+    
+        for line in file:
+        
+            line_count += 1
+
+    sorted_items_by_key = sorted(data_dict.items())
+
+
+    sorted_dict_by_key = dict(sorted_items_by_key)
+
+
+
+
+
+    sorted_dict_by_key = dict(sorted_items_by_key)
+
+
+    num=2
+    i=1
+    word=""
+    for key in sorted_dict_by_key:
+      if i>line_count:
+          break
+      else:
+        word=word+sorted_dict_by_key[i]+" "
+        i=i+num
+        num=num+1 
+    print(word)
+
+
+'''
+This Python code reads a file (coding_qual_input.txt) and extracts numbers and corresponding strings from each line using a regular expression. The extracted data is stored in a dictionary (result_dict) with the numbers as keys and strings as values. The dictionary is then sorted by keys, and the sorted result is stored in sorted_dict_by_key.
+
+The code then counts the number of lines in the file and uses a loop to generate a summary string (word) by concatenating the strings associated with the keys in a specific sequence. The sequence is determined by a numeric progression, where the initial value is 1, and it increments by 2 in each iteration.
+'''
---
+++ library/test_library/test_myfunctions.py
@@ -0,0 +1,5 @@
+from myfunctions import analyze_sentiment
+text=input("enter data")
+hello=str(analyze_sentiment(text))
+print(hello)
+
